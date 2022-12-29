55c5acc947b6488cbbb369dfc1c3b52b36dacd25
David Xu <david.xu@duke.edu>
2022-12-28 22:00:49 -0500

    building towers of hanoi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [towers, setTowers] = useState([3, 0, 0])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {towers.map((towerHeight, index) => (
+          <div className='hanoi' key={index}>
+            {towerHeight}
+            <div className='line'>
+            </div>
+          </div>
+        ))
+        }
+      </div>
     </div>
   );
 }
