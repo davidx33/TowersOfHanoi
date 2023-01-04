89ac74aff7824691392293352570299917703f2d
David Xu <david.xu@duke.edu>
2023-01-04 09:39:58 -0500

    haricut time

--- src/App.js
+++ src/App.js
@@ -5,25 +5,29 @@ import './App.css';
 
 function App() {
   const [towers, setTowers] = useState([5, 0, 0])
+  const [selectedTower, setSelectedTower] = useState();
+
+  function handleSelectTower(idx) {
+    setSelectedTower(idx)
+  }
 
   return (
     <div className="App">
       <div className='wrapper'>
         {towers.map((towerHeight, index) => (
-          <div className='hanoi' key={index}>
-            {towerHeight}
-            <div className='line'>
-            </div>
-
+          <div className={selectedTower === index ? 'hanoi' : 'selected'}
+          key={index} 
+          onClick={() => (handleSelectTower(index))}>
+          {towerHeight}
+          <div className='line'>
+          </div>
             <div className='disks'>
-
-            {[...new Array(towerHeight)].map((_, idx) => (
-              <div className='disc'
-                style={{ width: `${idx * 10 + 10}px` }}>
-              </div>
-            ))
-            }
-            
+              {[...new Array(towerHeight)].map((_, idx) => (
+                <div className='disc'
+                  style={{ width: `${idx * 10 + 10}px` }}>
+                </div>
+              ))
+              }
             </div>
           </div>
         ))
